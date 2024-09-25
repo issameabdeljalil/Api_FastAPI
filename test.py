d4b885924555261ce13be08d8e972560e5ba00d9
louis_lebreton <lebretonlouis@outlook.fr>
2024-09-25 13:02:16 +0200

    2nd commit

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Header
 import math
 import json
+import httpx
 
 from schema import User
 
 app = FastAPI()
-
 
 @app.get("/get_user/{user_prenom}/{user_nom}")
 async def recupere_user(user_nom: str, user_prenom: str): # a mettre dans l'URL
@@ -23,3 +23,13 @@ async def create_user(user: User):
         "prenom": user_prenom,
     }
 
+@app.get("/get_ville/{ville_nom}")
+async def recupere_ville(codePostal: str): # a mettre dans l'URL
+    url = f'https://geo.api.gouv.fr/communes?codePostal={codePostal}'
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+    
+    if response.status_code == 200: # succes
+        return response.json()  
+    else:
+        return {"error": "probleme avec API externe"}
